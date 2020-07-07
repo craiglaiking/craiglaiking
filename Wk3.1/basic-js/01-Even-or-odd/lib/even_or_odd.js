8defaed84a0c2f76207c657db7317a59cceeb4b0
Craig Lai King <craiglaiking@gmail.com>
2020-07-07 20:57:24 +0800

    Saved WK3.1 - Intro to JS

---
+++ Wk3.1/basic-js/01-Even-or-odd/lib/even_or_odd.js
@@ -0,0 +1,8 @@
+evenOrOdd = (number) => {
+  if (value%2 == 0)
+    return "even";
+  else
+    return "odd";
+  return number
+}
+  // TODO: this should return "even" if the number is even, "odd" otherwise
